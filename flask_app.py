aa645328f2d373c476e481c36a124b89fb54c206
Jhoan5aasdsadfasdas <sebastiandiaz4757@hotmail.com>
2024-04-13 09:56:23 -0500

    mejora de codigo

--- flask_app.py
+++ flask_app.py
@@ -1,40 +1,128 @@
 from flask import Flask, render_template, request, send_file
-from . import api_jobs
-from . import api_fake_jobs
+import requests
+
+# Peticion a api para obtener trabajos
+def search_jobs(query, location, lang, datePosted):
+    url = "https://jobs-api14.p.rapidapi.com/list"
+
+    querystring = {
+        "query": query,
+        "location": location,
+        "distance": "1.0",
+        "language": lang,
+        "datePosted": datePosted,
+        "employmentTypes": "fulltime;parttime;intern;contractor",
+        "index": "0"
+    }
+
+    headers = {
+        "X-RapidAPI-Key": "1b36c99d7dmshd9bfdc0ee6eececp104e5ajsn6bc4e59e68c5",
+        "X-RapidAPI-Host": "jobs-api14.p.rapidapi.com"
+    }
+
+    try:
+        response = requests.get(url, headers=headers, params=querystring)
+        response.raise_for_status()  # Raise an exception for 4xx or 5xx status codes
+    except requests.exceptions.RequestException as e:
+        return f'Error: {e}'
+
+    return response.json()
+
+def jobs_json():
+        return {
+  "jobs": [
+    {
+      "id": 1,
+      "title": "Trabajo 1",
+      "description": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis ducimus temporibus placeat sequi exercitationem odit illo sapiente cupiditate. Quisquam eveniet corrupti officiis voluptates deleniti sapiente libero voluptatum? Quasi, tempore placeat.",
+      "employmentType": "Remoto",
+      "jobProviders": [
+        {
+          "url": "",
+          "jobProvider": "proveedor1"
+        },
+        {
+          "url": "",
+          "jobProvider": "proveedor1"
+        },
+        {
+          "url": "",
+          "jobProvider": "proveedor2"
+        },
+        {
+          "url": "",
+          "jobProvider": "proveedor3"
+        }
+      ]
+    },
+    {
+      "id": 1,
+      "title": "Trabajo 2",
+      "description": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis ducimus temporibus placeat sequi exercitationem odit illo sapiente cupiditate. Quisquam eveniet corrupti officiis voluptates deleniti sapiente libero voluptatum? Quasi, tempore placeat.",
+      "employmentType": "Remoto",
+      "jobProvider": [
+        {
+          "url": "",
+          "jobProvider": "proveedor1"
+        },
+        {
+          "url": "",
+          "jobProvider": "proveedor1"
+        },
+        {
+          "url": "",
+          "jobProvider": "proveedor2"
+        },
+        {
+          "url": "",
+          "jobProvider": "proveedor3"
+        }
+      ]
+    },
+    {
+      "id": 1,
+      "title": "Trabajo 3",
+      "description": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis ducimus temporibus placeat sequi exercitationem odit illo sapiente cupiditate. Quisquam eveniet corrupti officiis voluptates deleniti sapiente libero voluptatum? Quasi, tempore placeat.",
+      "employmentType": "Remoto",
+      "jobProvider": [
+        {
+          "url": "",
+          "jobProvider": "proveedor1"
+        },
+        {
+          "url": "",
+          "jobProvider": "proveedor1"
+        },
+        {
+          "url": "",
+          "jobProvider": "proveedor2"
+        },
+        {
+          "url": "",
+          "jobProvider": "proveedor3"
+        }
+      ]
+    }
+  ]
+}
+
 
 app = Flask(__name__)
 
 @app.route('/', methods=('GET', 'POST'))
 def index():
-    if request.method == 'POST' and len(request.form['search']) > 1:
-        text = request.form['search']
-        return render_template('ofertas.html', query=text)
-    datos = api_fake_jobs.data()
-    return render_template('index.html', datos=datos)
-@app.route('/fake_response.json')
-def serve_json():
-    try:
-        # Ruta al archivo JSON local
-        ruta_archivo = 'fake_response.json'
-
-        # Utiliza Flask's send_file para enviar el archivo JSON
-        return send_file(ruta_archivo, mimetype='application/json')
-
-    except Exception as e:
-        # Si hay un error, devuelve un mensaje de error
-        return f"Error: {str(e)}"
-
-@app.route('/ofertas', methods=('GET', 'POST'))
-def ofertas():
     if request.method == 'POST':
         query = request.form['search']
         location = "Colombia"
         lang = "es"
         datePosted = "month"
-        response = api_jobs.search_jobs(query, location, lang, datePosted)
+        response = search_jobs(query, location, lang, datePosted)
         jobs = response['jobs']
-        return render_template('ofertas.html', jobs=jobs, query=query)
-    return render_template('ofertas.html')
+        return render_template('index.html', jobs=jobs, query=query)
+    jobs = jobs_json()
+    query = ""
+
+    return render_template('index.html', jobs=jobs['jobs'], query=query)
 
 if __name__ == '__main__':
     app.run(debug=True)
